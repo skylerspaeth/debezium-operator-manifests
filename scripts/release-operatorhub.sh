c3dc3b8ed9478771a8bf0d7de7b3c4562e30cb2f
Jakub Cechacek <jcechace@gmail.com>
2023-10-06 12:39:35 +0200

    DBZ-7010 Support for OKD/Openshift catalog in OH release script

--- scripts/release-operatorhub.sh
+++ scripts/release-operatorhub.sh
@@ -5,12 +5,12 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "$SCRIPT_DIR/functions.sh"
 checkDependencies
 
-OPTS=`getopt -o v:r:f --long version:,repodir:,force,push,push-remote: -n 'parse-options' -- "$@"`
+OPTS=`getopt -o v:k:o:f --long version:,k8s:,ocp:,force,push,push-remote: -n 'parse-options' -- "$@"`
 if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
 eval set -- "$OPTS"
 
 # Set defaults
-INPUT_DIR_BASE="$SCRIPT_DIR/../olm/bundles"
+INPUT_DIR_BASE="$(cd -- "$SCRIPT_DIR/../olm/bundles" && pwd )"
 FORCE=false
 PUSH=false
 PUSH_REMOTE="origin"
@@ -19,7 +19,8 @@ PUSH_REMOTE="origin"
 while true; do
   case "$1" in
     -v | --version )            BUNDLE_VERSION=$2;                  shift; shift ;;
-    -r | --repodir )            COMMUNITY_OPERATORS_REPO_DIR=$2;    shift; shift ;;
+    -k | --k8s )                K8_COMMUNITY_OPERATORS=$2;          shift; shift ;;
+    -o | --ocp )                OCP_COMMUNITY_OPERATORS=$2;         shift; shift ;;
     -f | --force )              FORCE=true;                         shift ;;
     --push-remote )             PUSH_REMOTE=$2;                     shift; shift ;;
     --push )                    PUSH=true;                          shift ;;
@@ -30,59 +31,77 @@ done
 
 INPUT_DIR="$INPUT_DIR_BASE/$BUNDLE_VERSION"
 OUTPUT_DIR_PATH="operators/debezium-operator"
-OUTPUT_DIR_BASE="$COMMUNITY_OPERATORS_REPO_DIR/$OUTPUT_DIR_PATH"
-OUTPUT_DIR="$OUTPUT_DIR_BASE/$BUNDLE_VERSION"
-BRANCH="debezium-operator.v$BUNDLE_VERSION"
 
-if [[ ! -d "$COMMUNITY_OPERATORS_REPO_DIR" ]]; then
-  echo "Community Operators repo directory $COMMUNITY_OPERATORS_REPO_DIR does not exist!"
-  exit 1
+function release() {
+    community_operators="$1"
+    community_operators_url="$2"
+    output_dir_base="$community_operators/$OUTPUT_DIR_PATH"
+    output_dir="$output_dir_base/$BUNDLE_VERSION"
+    ci_input_file="$INPUT_DIR_BASE/$3"
+    ci_output_file="$output_dir_base/ci.yaml"
+
+    if [[ ! -d "$community_operators" ]]; then
+      echo "Community Operators repo directory $community_operators does not exist!"
+      exit 1
+    fi
+
+    if [[ -d "$output_dir" && "$FORCE" = true ]]; then
+      echo "Removing exiting bundle directory '$output_dir'"
+      rm -rf "$output_dir"
+    fi
+
+
+    if [[ -d "$output_dir" ]]; then
+      echo "Directory $output_dir already exists!"
+      echo "Use -f / --force to overwrite"
+      exit 2
+    fi
+
+    echo ""
+    echo "Copying OLM bundle to Community Operators"
+    echo "Source dir: $INPUT_DIR"
+    echo "Target dir: $output_dir"
+    echo ""
+
+    # Copy bundle manifest and ci file to community operators repository
+    if [[ ! -d "$output_dir_base" ]]; then
+      mkdir -p "$output_dir_base"
+    fi
+    cp -r "$INPUT_DIR" "$output_dir"
+    cp "$ci_input_file" "$ci_output_file"
+
+    # Create branch and commit changes to Community Operators
+    pushd "$community_operators"
+    echo "Creating new branch '$BUNDLE_VERSION' in Community Operators repository"
+    git switch -c "$BUNDLE_VERSION" main
+    echo "Committing bundle to Community Operators repository"
+    git add "$OUTPUT_DIR_PATH"
+    git add "$ci_output_file"
+    echo ""
+    echo "Following change will be committed:"
+    git status
+    git commit -s -m "operator debezium-operator ($BUNDLE_VERSION)"
+
+
+    if [[ "$PUSH" = true ]]; then
+      echo "Pushing changes to remote repository"
+      git push "$PUSH_REMOTE" "$BUNDLE_VERSION"
+
+      echo ""
+      echo "Done! Review changes and open PR to the main branch of $community_operators_url"
+    else
+      echo ""
+      echo "Done! Push changes and open PR to the main branch of $community_operators_url"
+    fi
+
+    popd
+}
+
+
+if [[ ! -z "${K8_COMMUNITY_OPERATORS:-}" ]]; then
+    release "$K8_COMMUNITY_OPERATORS" "https://github.com/k8s-operatorhub/community-operators" "ci.yaml"
 fi
 
-if [[ -d "$OUTPUT_DIR" && "$FORCE" = true ]]; then
-  echo "Removing exiting bundle directory '$OUTPUT_DIR'"
-  rm -rf "$OUTPUT_DIR"
+if [[ ! -z "${OCP_COMMUNITY_OPERATORS:-}" ]]; then
+    release "$OCP_COMMUNITY_OPERATORS" "https://github.com/redhat-openshift-ecosystem/community-operators-prod" "ci.openshift.yaml"
 fi
-
-
-if [[ -d "$OUTPUT_DIR" ]]; then
-  echo "Directory $OUTPUT_DIR already exists!"
-  echo "Use -f / --force to overwrite"
-  exit 2
-fi
-
-echo ""
-echo "Copying OLM bundle to Community Operators"
-echo "Source dir: $INPUT_DIR"
-echo "Target dir: $OUTPUT_DIR"
-echo ""
-
-# Copy bundle manifest to community operators repository
-if [[ ! -d "$OUTPUT_DIR_BASE" ]]; then
-  mkdir -p "OUTPUT_DIR_BASE"
-fi
-cp -r "$INPUT_DIR" "$OUTPUT_DIR_BASE"
-
-
-# Create branch and commit changes to Community Operators
-pushd "$COMMUNITY_OPERATORS_REPO_DIR"
-echo "Creating new branch '$BRANCH' in Community Operators repository"
-git switch -c "$BRANCH" main
-echo "Committing bundle to Community Operators repository"
-git add "$OUTPUT_DIR_PATH"
-git commit -s -m "operator debezium-operator ($BUNDLE_VERSION)"
-
-
-if [[ "$PUSH" = true ]]; then
-  echo "Pushing changes to remote repository"
-  git push "$PUSH_REMOTE" "$BRANCH"
-
-  echo ""
-  echo "Done! Review changes and open PR to the main branch of https://github.com/k8s-operatorhub/community-operators"
-else
-  echo ""
-  echo "Done! Push changes and open PR to the main branch of https://github.com/k8s-operatorhub/community-operators"
-fi
-
-
-popd "$COMMUNITY_OPERATORS_REPO_DIR"
